7d9f39dc8de4c50f1233cb15673d066ef8b9a8e6
matias2312 <sgovia.matias@gmail.com>
2022-09-25 13:04:19 -0300

    modules

---
+++ src/main/resources/static/web/scripts/transfer.js
@@ -0,0 +1,70 @@
+const app = Vue.
+createApp({
+    data() {
+        return {
+            clients: [],
+            accounts:[],
+            amount:0,
+            description:"",           
+            accountOrigin:"",
+            accountDestiny:"",
+            accountOrigin2:"",
+            own:"own",
+            third:"third",            
+        }
+    },
+    
+    created(){
+      this.clientData()
+     
+    },
+    methods: {
+        clientData(){
+            axios.get('/api/clients/current')
+            .then(response => {
+                this.clients = response.data;
+                this.accounts = this.clients.accounts.sort((a,b) =>{
+                    return a.id - b.id
+               });
+            })           
+            .catch(function (error) {
+                console.log(error);
+            })
+           
+        },
+        newDate(creationDate){
+            creationDate = new Date(creationDate).toLocaleDateString();
+            return creationDate; 
+        },
+        newNumber(balance){ 
+            balance = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(balance);
+            return balance
+        },
+  
+        logout(){
+            axios.post('/api/logout')
+            .then(response => location.href = "/index.html")
+        },
+        newTransfer(){
+            axios.post("/api/clients/current/transactions",`accountOrigin=${this.accountOrigin}&accountDestiny=${this.accountDestiny}&amount=${this.amount}&description=${this.description}`,
+                {headers:{'content-type':'application/x-www-form-urlencoded'}})
+                .then(response =>
+                    Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: 'Transfer Create',
+                    showConfirmButton: false,
+                    timer: 1500
+                  }))
+                .then(response => window.location.reload())
+                .catch(error => 
+                    Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: error.response.data
+                  }))
+        }
+    }
+  
+
+}).mount('#app')
